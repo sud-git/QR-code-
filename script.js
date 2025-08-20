b7430907ef3b0a83caae082af0bd84e37fb58862
aj <aa@gmail.com>
2025-08-20 18:10:25 +0530

    Initial commit - QR Code Generator

---
+++ script.js
@@ -0,0 +1,47 @@
+let btn = document.querySelector(".button");
+
+function generate(user_input) {
+  const qrContainer = document.querySelector(".qr-code");
+  qrContainer.innerHTML = ""; // clear old QR
+  qrContainer.style.display = "block";
+
+  // Generate QR
+  let qrcode = new QRCode(qrContainer, {
+    text: user_input.value,
+    width: 180,
+    height: 180,
+    colorDark: "#000000",
+    colorLight: "#ffffff",
+    correctLevel: QRCode.CorrectLevel.H
+  });
+
+  // Add Download button
+  setTimeout(() => {
+    let img = qrContainer.querySelector("img");
+    let canvas = qrContainer.querySelector("canvas");
+
+    let downloadBtn = document.createElement("button");
+    let downloadLink = document.createElement("a");
+    downloadLink.setAttribute("download", "qr_code.png");
+    downloadLink.innerText = "Download";
+
+    if (img && img.getAttribute("src")) {
+      downloadLink.href = img.src;
+    } else if (canvas) {
+      downloadLink.href = canvas.toDataURL();
+    }
+
+    downloadBtn.appendChild(downloadLink);
+    qrContainer.appendChild(downloadBtn);
+  }, 300);
+}
+
+btn.addEventListener("click", () => {
+  let user_input = document.querySelector("#input_text");
+  if (user_input.value.trim() !== "") {
+    generate(user_input);
+  } else {
+    document.querySelector(".qr-code").style.display = "none";
+    console.log("Not valid input");
+  }
+});
